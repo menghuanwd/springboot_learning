4c916f1151260a791aa34a8e9c34f691dc4b5092
menghuanwd <651019063@qq.com>
2021-01-08 15:57:16 +0800

    learn

---
+++ src/main/java/com/menghuanwd/springboot/SpringbootLearnningApplication.java
@@ -0,0 +1,13 @@
+package com.menghuanwd.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearnningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearnningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menghuanwd/springboot/SpringbootLearnningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menghuanwd.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearnningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
